85c85931c167a009211b0ed87c5a9370953d5040
Olorunfunmi12 <olsho12@morgan.edu>
2026-03-02 13:54:59 -0500

    Add Python beginner quiz
    Initialize a new Git repository for the Python quiz project and push to GitHub.

---
+++ python_quiz.jsx
@@ -0,0 +1,7 @@
+mkdir python-quiz && cd python-quiz
+# paste your python_quiz.jsx file here
+git init
+git add .
+git commit -m "Add Python beginner quiz"
+git remote add origin https://github.com/Olorunfunmi12/python-quiz.git
+git push -u origin main
